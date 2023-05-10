bb99e41c5c542a76e3d0a866e46ae692cf2919b5
 S-Ramanan <ramananmvm@gmail.com>
2023-05-10 12:21:07 +0530

    initial commit

---
+++ CODES/DATATYPES.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.5.1;
+contract datatype{
+    //datatypes
+    uint256 x=5;
+    bool mybool=true;
+    string mystring="blockchain";
+    
+    bytes32 mybytes="hello";
+
+    //arrays of different types
+    uint256[] myunitarray= [1,2,3];
+    bool[] myboolarray=[true,false,false];
+    string[] mystringarr=["a","b"];
+
+}
